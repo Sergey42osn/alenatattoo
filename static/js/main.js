6a2918fd715eae317dfbc6f2230e606331f63805
Sergey42osn <sergey42osn@gmail.com>
2024-12-11 17:10:48 +0700

    444

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,18 @@
-(function () {
+(function (w) {
 
     var MAIN = {
 
+        tg: null,
+
+        page_serche_event: function(){
+
+            console.log(MAIN);
+
+        },
+
         init_obj: function(){
 
-            
+            MAIN.tg = window.Telegram.WebApp;
                 
         },
 
@@ -18,7 +26,9 @@
 
     window.addEventListener('load', function() {
 
-        MAIN.init();
+        
     });
+
+    MAIN.init();
 
 })();
